3445a36cccc95d60b172bae1daca8fa9e0a43456
Alan <acasasar@everis.com>
2019-07-08 22:26:18 +0200

    [FEATURE][MONGO CONNECTION]
    Mongo db connection was implemented
    Global variables was added

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
+const mongoose = require('mongoose');
+require('./config/db');
 const express = require('express');
 const path = require('path');
 const router = require('./routes');
-
 const app = express();
+require('dotenv').config({path: 'global_variables.env'});
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/',
     router()
 );
+app.use(cookieParser());
+
+app.use(session({
+    secret: process.env.SECRET_SEED,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+        mongooseConnection: mongoose.connection
+    })
+}));
 
 app.listen(4000);
 
